af0d106f05a9af3fbd1873c92e7e14d9b4eff96e
Miguel Gutierrez <miguel145973@gmail.com>
2025-02-17 17:57:24 -0600

    Starting TabBarView

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        //Icono de la izquierda
+        leading: Icon(
+          Icons.menu,
+          color: Colors.gray[800] ,),
+        actions: [
+          Icon()
+        ],
+        ),
+      
+    );
+  
+  }
+}
